2707ba2be389408a7c740dd83435f9492d2c6853
Kaveesha Hettige <2021is046@stu.ucsc.cmb.ac.lk>
2024-07-19 11:00:25 +0530

    implemented findall ,findbycategory and addQuestion

---
+++ Quizzz/src/main/java/com/example/Quizzz/Question.java
@@ -0,0 +1,22 @@
+package com.example.Quizzz;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String question;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String right_answer;
+    private String difficulty_level;
+    private String category;
+}
---
+++ Quizzz/src/main/java/com/example/Quizzz/controller/QuestionController.java
@@ -0,0 +1,31 @@
+package com.example.Quizzz.controller;
+
+import com.example.Quizzz.Question;
+import com.example.Quizzz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("addQuestion")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+}
